44f73b387676dc9f4028a102ed133029bb456586
jansm04 <jan.smailbegovic@gmail.com>
2024-09-28 13:57:13 -0700

    implement GET requests

--- backend/src/routes/volunteerRoutes.js
+++ backend/src/routes/volunteerRoutes.js
@@ -1,1 +1,8 @@
-//volunteerRoutes.js
+const router = express('router')
+const controller = require('../controllers/volunteerController')
+
+// get volunteer profile by email
+router.post('/volunteer-profile', controller.getVolunteerProfile) 
+
+// get all volunteers
+router.get('/volunteers', controller.getVolunteers) 

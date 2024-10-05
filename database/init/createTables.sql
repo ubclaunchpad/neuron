c4ac6159a9e583a670acbdaeddf2edf6db07eee5
Leo <leowang801@gmail.com>
2024-10-05 12:17:48 -0700

    switch id to varchar(255) from INT

--- database/init/createTables.sql
+++ database/init/createTables.sql
@@ -1,24 +1,31 @@
 -- Paste all 'create' SQL commands here
 
 create table users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
     role VARCHAR(5) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+create table user_session (
+    id VARCHAR(255) PRIMARY KEY,
+    expires_at DATETIME NOT NULL,
+    fk_user_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (fk_user_id) REFERENCES users(user_id)
+);
+
 create table instructors (
-	instructor_id INT PRIMARY KEY AUTO_INCREMENT, 
-    fk_user_id INT,         
+	instructor_id VARCHAR(255) PRIMARY KEY, 
+    fk_user_id VARCHAR(255),         
     f_name VARCHAR(15) NOT NULL,
     l_name VARCHAR(15) NOT NULL,
     FOREIGN KEY (fk_user_id) REFERENCES users(user_id)
 );
 
 create table class (
-	class_id INT PRIMARY KEY,
-    fk_instructor_id INT NOT NULL,
+	class_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_instructor_id VARCHAR(255) NOT NULL,
     class_name VARCHAR(64) NOT NULL,
     instructions VARCHAR(150),
     zoom_link VARCHAR(3000) NOT NULL,
@@ -28,8 +35,8 @@ create table class (
 );
 
 create table volunteers (
-	volunteer_id INT PRIMARY KEY, 
-    fk_user_id INT,         
+	volunteer_id VARCHAR(255) PRIMARY KEY, 
+    fk_user_id VARCHAR(255),         
     f_name VARCHAR(15) NOT NULL,
     l_name VARCHAR(15) NOT NULL,
     total_hours INT NOT NULL,
@@ -40,16 +47,16 @@ create table volunteers (
 );
 
 create table admins (
-	admin_id INT PRIMARY KEY, 
-    fk_user_id INT,         
+	admin_id VARCHAR(255) PRIMARY KEY, 
+    fk_user_id VARCHAR(255),         
     f_name VARCHAR(15) NOT NULL,
     l_name VARCHAR(15) NOT NULL,
     FOREIGN KEY (fk_user_id) REFERENCES users(user_id)
 );
 
 create table availability (
-	availability_id INT PRIMARY KEY,
-    fk_volunteer_id INT NOT NULL,
+	availability_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_volunteer_id VARCHAR(255) NOT NULL,
     day_of_week INT NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
@@ -57,13 +64,13 @@ create table availability (
 );
 
 create table volunteer_class (
-	fk_volunteer_id INT REFERENCES volunteers(volunteer_id),
-    fk_class_id INT REFERENCES class(class_id),
+	fk_volunteer_id VARCHAR(255) REFERENCES volunteers(volunteer_id),
+    fk_class_id VARCHAR(255) REFERENCES class(class_id),
     PRIMARY KEY (fk_volunteer_id, fk_class_id)
 );
 
 create table schedule (
-	schedule_id INT PRIMARY KEY,
+	schedule_id INT PRIMARY KEY AUTO_INCREMENT,
     fk_class_id INT NOT NULL,
     day_of_week INT NOT NULL,
     start_time TIME NOT NULL,
@@ -72,23 +79,23 @@ create table schedule (
 );
 
 create table shifts (
-	fk_volunteer_id INT REFERENCES volunteers(volunteer_id),
-    fk_class_id INT REFERENCES class(class_id),
+	fk_volunteer_id VARCHAR(255) REFERENCES volunteers(volunteer_id),
+    fk_schedule_id INT REFERENCES schedule(schedule_id),
     shift_date DATE NOT NULL,
     duration INT NOT NULL,
-    PRIMARY KEY (fk_volunteer_id, fk_class_id, shift_date)
+    PRIMARY KEY (fk_volunteer_id, fk_schedule_id, shift_date)
 );
 
 CREATE TABLE shift_coverage_request (
-    request_id INT PRIMARY KEY,                     
-    fk_volunteer_id INT NOT NULL,                   
-    fk_class_id INT NOT NULL,
+    request_id VARCHAR(255) PRIMARY KEY,                     
+    fk_volunteer_id VARCHAR(255) NOT NULL,                   
+    fk_schedule_id INT NOT NULL,
     shift_date DATE NOT NULL,                       
-    covered_by INT NOT NULL,
+    covered_by VARCHAR(255) NOT NULL,
     fulfilled BOOLEAN,
-    FOREIGN KEY (fk_volunteer_id, fk_class_id, shift_date)
-        REFERENCES shifts(fk_volunteer_id, fk_class_id, shift_date),  
+    FOREIGN KEY (fk_volunteer_id, fk_schedule_id, shift_date)
+        REFERENCES shifts(fk_volunteer_id, fk_schedule_id, shift_date),
     FOREIGN KEY (covered_by)
-        REFERENCES volunteers(volunteer_id)        
+        REFERENCES volunteers(volunteer_id)
 );
 

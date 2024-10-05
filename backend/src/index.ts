8004487769bf836aa19fbba2f3c18f1ad0cc6eba
Jessie Shang <17jessies@gmail.com>
2024-10-04 23:46:58 -0700

    Revert "BE Auth - support for auth, middleware and db integration to handle sessions"

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,8 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import bodyParser from "body-parser";
-import {authRouter} from "./routes/authRouter.js";
-import {authMiddleware} from "./middlewares/authMiddleware.js";
 
-// set the default port to be 3001
+// set default port to be 3001
 const PORT: number = parseInt(process.env.PORT || '3001', 10);
 const app = express();
 
@@ -13,16 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(authMiddleware);
-app.use(authRouter);
-
-app.get('/', (_: Request, res: Response) =>  {
-  if (!res.locals.user) {
-    // This isn't needed - but for types. You can make a helper function for this
-    res.send({ message: 'Hello Team Neuron! Please login' });
-    return;
-  }
-  res.send({ message: `Hello Team Neuron! You are logged in as ${res.locals.user.email}` });
+app.get('/', (req: Request, res: Response) =>  {
+  res.send({ message: "Hello Team Neuron!" });
 });
 
 app.listen(PORT, () => {

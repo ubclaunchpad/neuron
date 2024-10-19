b9f0d840dbf910710d34a7aa5277d9d141217446
Jessie Shang <17jessies@gmail.com>
2024-10-19 14:07:01 -0700

    Update index.ts routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import classRouter from "./routes/classRoutes.js"
 import shiftRouter from "./routes/shiftRoutes.js"
-
-// volunteer routes
 import volunteerRouter from './routes/volunteerRoutes.js';
 import instructorRouter from './routes/instructorRoutes.js';
 
@@ -17,17 +15,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/classes", classRouter);
-app.use("/classes/shifts", shiftRouter);
-
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: "Hello Team Neuron!" });
 });
 
-
 // define all routes
 app.use('/volunteer', volunteerRouter);
 app.use('/instructors', instructorRouter);
+app.use("/classes", classRouter);
+app.use("/classes/shifts", shiftRouter);
 
 app.listen(PORT, () => {
   console.log(`Neuron backend server listening on ${PORT}`);

8c54b649c862d6d0156e39b38d4d3ddd5ff91704
Brianna Shen <briannashen905@gmail.com>
2024-10-19 13:10:02 -0700

    Update types.ts
    - Availability Interface
    - Volunteer Interface
    - User Interface
    - Class Interface
    - Shift Interface

--- backend/src/common/types.ts
+++ backend/src/common/types.ts
@@ -5,3 +5,61 @@ export interface Instructor {
     l_name: string; 
     email: string;      
   }
+
+  // Availability
+  export interface Availability {
+    day_of_week: day_of_week; 
+    start_time: string;
+    end_time: string;       
+    availability_id?: string;       
+  }
+
+  enum day_of_week {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+
+  // Volunteer
+  export interface Volunteer {
+    volunteer_id?: string; 
+    fk_user_id?: string;   // Foreign key (optional, since it might be nullable)
+    f_name: string;       
+    l_name: string; 
+    email: string;      
+    total_hours: number;
+    class_preferences: string;
+    bio?: string;
+    activate: boolean;
+  }
+
+  // User
+  export interface User {
+    user_id?: string; 
+    email: string;      
+    password: string;
+    role: "admin" | "volunteer" | "intructor";
+  }
+
+  // Class
+  export interface Class {
+    class_id?: number; 
+    fk_instructor_id?: string;   // Foreign key (optional, since it might be nullable)
+    class_name: string;       
+    instructions?: string; 
+    zoom_link?: string;      
+    start_date: string;
+    end_date?: string;
+  }
+
+  // Shift
+  export interface Shift {
+    fk_volunteer_id: string;
+    fk_schedule_id: number;
+    shift_date: string;
+    duration: number;   
+  }

d742fdaf560713c7fc5bd573be5b2ed5c960bea0
jansm04 <jan.smailbegovic@gmail.com>
2024-10-26 12:14:58 -0700

    use shift interface

--- backend/src/controllers/shiftController.ts
+++ backend/src/controllers/shiftController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ShiftModel from '../models/shiftModel.js';
+import { Shift } from '../common/interfaces.js';
 
 const shiftModel = new ShiftModel();
 
@@ -7,25 +8,23 @@ const shiftModel = new ShiftModel();
 const sqlDateRegex = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 
 async function getShiftInfo(req: Request, res: Response){
-    const fk_volunteer_id = req.body.volunteerID; 
-    const fk_schedule_id = req.body.scheduleID;
-    const shift_date = req.body.shiftDate;
+    const shift: Shift = req.body;
 
-    if (!fk_volunteer_id || !fk_schedule_id || !shift_date) {
+    if (!shift.fk_volunteer_id || !shift.fk_schedule_id || !shift.shift_date) {
         return res.status(400).json({
-            error: "Missing required fields. 'volunteerID', 'scheduleID' and 'shiftDate' are required."
+            error: "Missing required fields. 'fk_volunteer_id', 'fk_schedule_id' and 'shift_date' are required."
         });
     }
 
     // check if the input matches the regex
-    if (!sqlDateRegex.test(shift_date)) {
+    if (!sqlDateRegex.test(shift.shift_date)) {
         return res.status(400).json({
             error: "'shiftDate' must be a valid date of the format 'YYYY-MM-DD'"
         });
     }
     
     try {
-        const shift_info = await shiftModel.getShiftInfoFromDB(fk_volunteer_id, fk_schedule_id, shift_date);
+        const shift_info = await shiftModel.getShiftInfoFromDB(shift.fk_volunteer_id, shift.fk_schedule_id, shift.shift_date);
         res.status(200).json(shift_info[0]);
     } catch (error: any) {
         return res.status(500).json({
@@ -56,23 +55,23 @@ async function getShiftsByVolunteerId(req: Request, res: Response) {
 
 // get all the shifts on a given date
 async function getShiftsByDate(req: Request, res: Response) {
-    const shift_date = req.body.shiftDate;
+    const shift: Shift = req.body;
 
-    if (!shift_date) {
+    if (!shift.shift_date) {
         return res.status(400).json({
-            error: "Missing required field: 'shiftDate'"
+            error: "Missing required field: 'shift_date'"
         });
     }
 
     // check if the input matches the regex
-    if (!sqlDateRegex.test(shift_date)) {
+    if (!sqlDateRegex.test(shift.shift_date)) {
         return res.status(400).json({
             error: "'shiftDate' must be a valid date of the format 'YYYY-MM-DD'"
         });
     }
 
     try {
-        const shifts = await shiftModel.getShiftsByDate(shift_date);
+        const shifts = await shiftModel.getShiftsByDate(shift.shift_date);
         res.status(200).json(shifts);
     } catch (error: any) {
         return res.status(500).json({

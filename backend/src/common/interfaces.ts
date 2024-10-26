a09351554d63df27a10c4c70b5e75dea3734ca42
Brianna Shen <briannashen905@gmail.com>
2024-10-25 23:15:25 -0700

    Update interfaces.ts
    - Removed comments
    - Added a new line
    - Removed an interface
    - Edited roles in the User interface

--- backend/src/common/interfaces.ts
+++ backend/src/common/interfaces.ts
@@ -1,12 +1,10 @@
-// User
 export interface User {
   user_id?: string;
   email: string;
   password: string;
-  role: "admin" | "volunteer" | "instructor";
+  role: "ADMIN" | "VOLUN" | "INSTR";
 }
 
-// Volunteer
 export interface Volunteer {
   volunteer_id?: string;
   fk_user_id?: string;   // Foreign key (optional, since it might be nullable)
@@ -27,7 +25,6 @@ export interface Instructor {
   email: string;
 }
 
-// Availability
 export interface Availability {
   day: day_of_week;
   start_time: string; // MySQL uses TIME type, example: '11:12' is recognized as '11:12:00'
@@ -45,7 +42,6 @@ enum day_of_week {
   Sunday
 }
 
-// Class
 export interface Class {
   class_id?: number;
   fk_instructor_id?: string;   // Foreign key (optional, since it might be nullable)
@@ -56,23 +52,9 @@ export interface Class {
   end_date?: string;
 }
 
-// Shift
 export interface Shift {
   fk_volunteer_id: string;
   fk_schedule_id: number;
   shift_date: string;
   duration: number;
-}export interface Availability {
-  day: day_of_week;
-  start_time: string; // MySQL uses TIME type, example: '11:12' is recognized as '11:12:00'
-  end_time: string;
-  availability_id?: string;
-}
-
-export interface Instructor {
-  instructor_id: string;
-  fk_user_id?: string;   // Foreign key (optional, since it might be nullable)
-  f_name: string;
-  l_name: string;
-  email: string;
 }

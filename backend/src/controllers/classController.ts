416a58097189fbc05af0af61045d35f7d642a29d
Alec Situ <aasitu@gmail.com>
2024-11-23 11:51:07 -0800

    get classes by day api (#13)

--- backend/src/controllers/classController.ts
+++ backend/src/controllers/classController.ts
@@ -57,4 +57,28 @@ export default class ClassesController {
 			});
 		}
 	}
+
+  public async getClassesByDay(req: Request, res: Response) {
+    const day = req.params.day;
+    const classModel = new ClassesModel();
+    if (!day) {
+      return res.status(400).json({
+        error: "Missing required parameter: 'day'",
+      });
+    }
+    const regex = /^\d{4}-\d{2}-\d{2}$/; // YYYY-MM-DD
+    if (!regex.test(day)) {
+      return res.status(400).json({
+        error: "Invalid day format. Please use YYYY-MM-DD.",
+      });
+    }
+    try {
+      const classes = await classModel.getClassesByDay(day);
+      res.status(200).json(classes);
+    } catch (error: any) {
+      return res.status(error.status).json({
+        error: error.message,
+      });
+    }
+  }
 }

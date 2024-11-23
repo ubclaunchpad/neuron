d71e4cb62fafa27c18c492cc80ef72eb25c2f4d4
gigabite-pro <rajmachawal19@gmail.com>
2024-11-22 22:00:43 -0800

    merging from main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "notyf/notyf.min.css";
-import Classes from "./pages/classes";
-import VolunteerProfile from "./pages/volunteerProfile";
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { isAuthenticated } from "./api/authService";
->>>>>>> origin/main
 import VolunteerDash from "./pages/volunteerDash";
 import VolunteerSignup from "./pages/VolunteerSignup";
 import VolunteerLogin from "./pages/VolunteerLogin";
@@ -19,14 +11,17 @@ import VolunteerProfile from "./pages/volunteerProfile";
 import AdminVerify from "./pages/AdminVerify";
 
 function App() {
-
     const [isVolunteer, setIsVolunteer] = useState(false);
 
     useEffect(() => {
         const checkAuth = async () => {
             try {
                 const authResponse = await isAuthenticated();
-                if (authResponse && authResponse.volunteer && authResponse.volunteer.volunteer_id) {
+                if (
+                    authResponse &&
+                    authResponse.volunteer &&
+                    authResponse.volunteer.volunteer_id
+                ) {
                     setIsVolunteer(true);
                 } else {
                     setIsVolunteer(false);
@@ -45,16 +40,39 @@ function App() {
                 <Routes>
                     <Route path="/auth/signup" element={<VolunteerSignup />} />
                     <Route path="/auth/login" element={<VolunteerLogin />} />
-                    <Route path="/auth/forgot-password" element={<VolunteerForgotPassword />}/>
-                    <Route path="/auth/reset-password" element={<VolunteerResetPassword />}/>
-                    {isVolunteer && ( <>
+                    <Route
+                        path="/auth/forgot-password"
+                        element={<VolunteerForgotPassword />}
+                    />
+                    <Route
+                        path="/auth/reset-password"
+                        element={<VolunteerResetPassword />}
+                    />
+                    {isVolunteer && (
+                        <>
                             <Route path="/" element={<VolunteerDash />} />
-                            <Route path="/volunteer/classes" element={<Classes />} />
-                            <Route path="/volunteer/my-profile" element={<VolunteerProfile />} />
-                            <Route path="/volunteer/schedule" element={<VolunteerDash />}/>
-                            <Route path="/volunteer/classes" element={<Classes />} /> </>
+                            <Route
+                                path="/volunteer/classes"
+                                element={<Classes />}
+                            />
+                            <Route
+                                path="/volunteer/my-profile"
+                                element={<VolunteerProfile />}
+                            />
+                            <Route
+                                path="/volunteer/schedule"
+                                element={<VolunteerDash />}
+                            />
+                            <Route
+                                path="/volunteer/classes"
+                                element={<Classes />}
+                            />{" "}
+                        </>
                     )}
-                    <Route path="/admin/verify-volunteers" element={<AdminVerify />} />
+                    <Route
+                        path="/admin/verify-volunteers"
+                        element={<AdminVerify />}
+                    />
                 </Routes>
             </BrowserRouter>
         </div>

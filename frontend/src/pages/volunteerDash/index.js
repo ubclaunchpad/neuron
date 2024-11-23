452000ef599d1167f3a334bf4e06275aa42953ff
alecishuman <aasitu@gmail.com>
2024-11-23 09:09:33 -0800

    page layout

--- frontend/src/pages/volunteerDash/index.js
+++ frontend/src/pages/volunteerDash/index.js
@@ -6,25 +6,51 @@ import { useNavigate } from "react-router-dom";
 import VolunteerLayout from "../../components/volunteerLayout";
 import { getHelloWorld } from "../../api/homePageService";
 import { isAuthenticated } from "../../api/authService";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForwardIos";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 function VolunteerDash() {
   const [loading, setLoading] = useState(true);
+  const [checkIn, setCheckIn] = useState(true);
   const [data, setData] = useState(null);
   const progressCompleted = useRef(null);
   const progressUpcoming = useRef(null);
   const navigate = useNavigate();
 
+  const checkInItem = () => {
+    return checkIn ? (
+      <div className="dash-check-in">
+        <div className="dash-check-in-content">
+          <div className="dash-check-in-title">Check In</div>
+          <div className="dash-check-in-content">
+            Strength & Balance Level 2 • 12:30 PM - 01:00 PM
+          </div>
+        </div>
+        <ArrowForwardIcon sx={{ fontSize: 30 }} />
+      </div>
+    ) : (
+      <div className="dash-next-check-in">
+        <div className="dash-next-title">Next Check-In in</div>
+        <div className="dash-next-time">
+          <span className="dash-next-time-num">0</span> days{" "}
+          <span className="dash-next-time-num">1</span> hours{" "}
+          <span className="dash-next-time-num">30</span> minutes
+        </div>
+      </div>
+    );
+  };
+
   useEffect(() => {
-    isAuthenticated()
-      .then((response) => {
-        console.log(response);
-        if (!response.isAuthenticated) {
-          navigate("/auth/login");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    // isAuthenticated()
+    //   .then((response) => {
+    //     console.log(response);
+    //     if (!response.isAuthenticated) {
+    //       navigate("/auth/login");
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
     getHelloWorld()
       .then((data) => {
         setData(data.message);
@@ -45,7 +71,10 @@ function VolunteerDash() {
     <VolunteerLayout pageTitle="Dashboard">
       <div className="dash-container">
         <div className="dash-col-card dash-grid-item">
-          <div className="dash-card-title">Volunteer Hours</div>
+          <div className="dash-card-title">
+            Volunteer Hours{" "}
+            <ArrowForwardIcon sx={{ fontSize: "16px", color: "#808080" }} />
+          </div>
           <div className="dash-hours-container">
             <div className="dash-hours">
               <h1 className="dash-completed-hours">20</h1>
@@ -59,8 +88,11 @@ function VolunteerDash() {
         </div>
         <div className="dash-col-card dash-grid-item">
           <div className="dash-card-header">
-            <div className="dash-card-title">Coverage Hours</div>
-            <div className="dash-coverage-help">H</div>
+            <div className="dash-card-title">
+              Coverage Hours{" "}
+              <ArrowForwardIcon sx={{ fontSize: "16px", color: "#808080" }} />
+            </div>
+            <HelpOutlineIcon sx={{ color: "var(--primary-blue)" }} />
           </div>
           <div>
             <span className="dash-hours-to-complete">6</span> to be completed
@@ -91,13 +123,20 @@ function VolunteerDash() {
           </div>
         </div>
         <div className="dash-col-card dash-grid-item">
-          <div className="dash-card-title">My Upcoming Shifts</div>
+          <div className="dash-card-title">
+            My Upcoming Shifts{" "}
+            <ArrowForwardIcon sx={{ fontSize: "16px", color: "#808080" }} />
+          </div>
         </div>
 
         <div className="dash-bottom-right dash-grid-item">
           <div className="dash-col-card">
-            <div className="dash-card-title">Shifts In Need of Coverage</div>
+            <div className="dash-card-title">
+              Shifts In Need of Coverage{" "}
+              <ArrowForwardIcon sx={{ fontSize: "16px", color: "#808080" }} />
+            </div>
           </div>
+          {checkInItem()}
         </div>
       </div>
     </VolunteerLayout>

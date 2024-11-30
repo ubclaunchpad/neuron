69112cbf4ffc591f572762246cf9abff753ff844
Leo <leowang801@gmail.com>
2024-11-29 20:57:48 -0800

    adjust styling

--- frontend/src/components/classIcon/index.js
+++ frontend/src/components/classIcon/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./index.css";
 
-function ClassIcon({ imageUrl, level, title, time }) {
+function ClassIcon({ imageUrl, title, time }) {
   return (
     <div className="class-icon-card">
       <img src={imageUrl} alt="Class" className="class-image" />
       <div className="class-details">
-        <p className="class-level">{level}</p>
         <h3 className="class-title">{title}</h3>
         <p className="class-time">{time}</p>
       </div>

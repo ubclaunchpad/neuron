9da991970447d7c202e998b8b14e99992756e449
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-01-17 16:30:52 -0800

    double check mime type on backend

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,14 +1,11 @@
 import { Request, Response, Router } from "express";
-import multer from 'multer';
+import { imageUploadMiddleware } from "../config/fileUpload.js";
 import { getUserById, insertProfilePicture } from "../controllers/userController.js";
 
 const router = Router();
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 // create profile picture for a user
-router.post("/:user_id/upload", upload.single('image'), (req: Request, res: Response) => {
+router.post("/:user_id/upload", imageUploadMiddleware, (req: Request, res: Response) => {
     insertProfilePicture(req, res);
 });
 

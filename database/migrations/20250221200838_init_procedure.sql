055b3bbee09ab954cb2a877ea6c8e3228d1cd0f0
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-02-23 18:19:07 -0800

    migrations

--- database/migrations/20250221200838_init_procedure.sql
+++ database/migrations/20250221200838_init_procedure.sql
@@ -1,5 +1,4 @@
--- Procedure to get all shifts for a volunteer by month
-DROP PROCEDURE IF EXISTS GetShiftsByVolunteerIdAndMonth;
+-- migrate:up
 DELIMITER $$
 
 CREATE PROCEDURE GetShiftsByVolunteerIdAndMonth(
@@ -81,6 +80,7 @@ BEGIN
         MONTH(shifts.shift_date) = month 
         AND YEAR(shifts.shift_date) = year 
         AND shifts.fk_volunteer_id <> volunteer_id
+        AND shift_coverage_request.covered_by IS NULL
 
     UNION ALL
 
@@ -99,7 +99,10 @@ BEGIN
         shifts.fk_volunteer_id AS volunteer_id,
         shifts.checked_in AS checked_in,
         'my-coverage-requests' AS shift_type,
-        NULL AS coverage_status, -- No coverage status for 'my-coverage-request'
+        CASE 
+            WHEN shift_coverage_request.covered_by IS NOT NULL THEN 'resolved'
+            ELSE 'open'
+        END AS coverage_status, -- Coverage status indicator
         shift_coverage_request.request_id AS request_id -- Request ID for 'my-coverage-request'
     FROM 
         shifts
@@ -117,10 +120,10 @@ BEGIN
         shifts.fk_volunteer_id = volunteer_id 
         AND MONTH(shifts.shift_date) = month 
         AND YEAR(shifts.shift_date) = year
-        AND shift_coverage_request.covered_by IS NULL
     ORDER BY 
         shift_date ASC,
         start_time ASC;
 END$$
 
-DELIMITER ;
+-- migrate:down
+DROP PROCEDURE IF EXISTS GetShiftsByVolunteerIdAndMonth;

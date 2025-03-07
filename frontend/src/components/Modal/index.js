5580cabfc9011b1f24a098122edfe929ed774d07
Khai Phan <khaidoanhtkg@gmail.com>
2025-03-06 21:23:37 -0800

    Completed Unverified User Page

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -3,16 +3,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import close_button from "../../assets/images/button-icons/button-close-icon.png";
 
-function Modal ({ isOpen, onClose, children, width, height }) {
+function Modal ({ isOpen, onClose, children, width, height, showCloseBtn }) {
 
      if (!isOpen) {
           return null;
      } 
+     if (showCloseBtn === undefined || showCloseBtn === null) {
+          showCloseBtn = true;
+     }
      
      return ReactDOM.createPortal(
           <div className="modal-overlay" >
                <div className="modal-content" style={{width: width, height: height}}>
-                    <img alt="Close Icon" className="close-button" onClick={onClose} src={close_button}/>
+                    {showCloseBtn && (
+                         <img alt="Close Icon" className="close-button" onClick={onClose} src={close_button}/>
+                         )
+                    }
                     {children}
                </div>
           </div>,
--- frontend/src/pages/MemberManagement/index.js
+++ frontend/src/pages/MemberManagement/index.js
@@ -1,12 +1,47 @@
 import "./index.css";
+import { useEffect, useState } from "react";
+import { getUnverifiedVolunteers } from "../../api/adminService";
+import UnverifiedUsers from "../../components/UnverifiedUsers";
 
 const MemberManagement = () => {
+    const [unverifiedUsers, setUnverifiedUsers] = useState(null);
+    const subpages = [(<>a</>), (<>b</>), (<UnverifiedUsers unverifiedUsers={unverifiedUsers}/>)];
+    const [currentView, setCurrentView] = useState(subpages[0])
+
+    useEffect(async () => {
+        const unverifiedUsers_ = await getUnverifiedVolunteers();
+        setUnverifiedUsers(unverifiedUsers_.volunteers);
+        console.log(unverifiedUsers_.volunteers);
+    }, []);
+
+
+    function showView(view) {
+        setCurrentView(subpages[view]);
+    }
+
+    function renderNumUsers() {
+        if (!unverifiedUsers || unverifiedUsers.length === 0) return null;
+        const num = unverifiedUsers.length;
+        return (<span className="num-unv-user">{num}</span>);
+    }
+
     return (
         <main className="content-container">
             <div className="content-heading">
                 <h2 className="content-title">Member Management</h2>
             </div>
-            {/* Add content here */}
+
+            <div className="selection-bar">
+                <button className="selection-bar-view col-1" onClick={()=>showView(0)}>Volunteers</button>
+                <button className="selection-bar-view" onClick={()=>showView(1)}>Instructors</button>
+                <button className="selection-bar-view col-3 active" onClick={()=>showView(2)}>
+                    Unverified Users {renderNumUsers()}
+                </button>
+            </div>
+
+            {currentView}
+
+
         </main>
     );
 };

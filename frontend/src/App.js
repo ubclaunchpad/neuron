e0bdec3202fe6dee059618e34f2df67b4dd7bbce
shailahaggarson <saggarson321@gmail.com>
2025-03-08 00:31:43 -0800

    Finish merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,6 @@ import api from "./api/api";
 import SidebarLayout from "./components/SidebarLayout";
 import { useAuth } from "./contexts/authContext";
 import Classes from "./pages/Classes";
-import VolunteerProfile from "./pages/VolunteerProfile";
-import VolunteerNotVerified from "./pages/VolunteerNotVerified";
-import VolunteerDeactivated from "./pages/VolunteerDeactivated";
 import ClassPreferences from "./pages/ClassPreferences";
 import CoverageRequests from "./pages/CoverageRequests";
 import Dashboard from "./pages/Dashboard";
@@ -20,7 +17,12 @@ import Schedule from "./pages/Schedule";
 import Settings from "./pages/Settings";
 import Signup from "./pages/Signup";
 import VolunteerProfile from "./pages/VolunteerProfile";
-import AdminVolunteerProfile from "./pages/AdminVolunterProfile";
+import VolunteerResetPassword from "./pages/VolunteerResetPassword";
+import VolunteerSignup from "./pages/VolunteerSignup";
+import ClassPreferences from "./pages/ClassPreferences";
+import VolunteerNotVerified from "./pages/VolunteerNotVerified";
+import VolunteerDeactivated from "./pages/VolunteerDeactivated";
+
 
 function App() {
   const { isAuthenticated, isAdmin, isVolunteer, logout } = useAuth();
@@ -48,10 +50,10 @@ function App() {
         <Routes>
           {/* Public Routes */}
           <Route path="auth" element={<RouteGuard fallback={"/"} valid={!isAuthenticated} />}>
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="signup" element={<VolunteerSignup />} />
+            <Route path="login" element={<VolunteerLogin />} />
+            <Route path="forgot-password" element={<VolunteerForgotPassword />} />
+            <Route path="reset-password" element={<VolunteerResetPassword />} />
             <Route path="not-verified" element={<VolunteerNotVerified />} />
             <Route path="deactivated" element={<VolunteerDeactivated />} />
           </Route>

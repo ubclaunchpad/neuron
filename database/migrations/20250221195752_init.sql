0e0c76cc04fb57db69b429410aed536356995ef6
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-03-08 21:23:06 -0800

    update initial sql

--- database/migrations/20250221195752_init.sql
+++ database/migrations/20250221195752_init.sql
@@ -1,11 +1,10 @@
 -- migrate:up
 create table users (
-    user_id VARCHAR(255) PRIMARY KEY,
+    user_id CHAR(36) PRIMARY KEY,
     f_name VARCHAR(60) NOT NULL,
     l_name VARCHAR(60) NOT NULL,
     email VARCHAR(45) NOT NULL UNIQUE,
-    fk_image_id VARCHAR(36),
-    email VARCHAR(45) NOT NULL,
+    fk_image_id CHAR(36),
     password CHAR(60) BINARY NOT NULL,
     role ENUM('volunteer', 'admin', 'instructor') NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -14,15 +13,15 @@ create table users (
 );
 
 create table instructors (
-	instructor_id VARCHAR(255) PRIMARY KEY, 
-    f_name VARCHAR(15) NOT NULL,
-    l_name VARCHAR(15) NOT NULL,
+	instructor_id CHAR(36) PRIMARY KEY, 
+    f_name VARCHAR(60) NOT NULL,
+    l_name VARCHAR(60) NOT NULL,
     email VARCHAR(45) NOT NULL,
 );
 
 create table class (
 	class_id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_instructor_id VARCHAR(255) NOT NULL,
+    fk_instructor_id CHAR(36) NOT NULL,
     fk_image_id CHAR(36),
     class_name VARCHAR(64) NOT NULL,
     instructions VARCHAR(150),
@@ -37,8 +36,8 @@ create table class (
 );
 
 create table volunteers (
-	volunteer_id VARCHAR(255) PRIMARY KEY, 
-    fk_user_id VARCHAR(255),         
+	volunteer_id CHAR(36) PRIMARY KEY, 
+    fk_user_id CHAR(36),      
     p_name VARCHAR(45),
     total_hours INT NOT NULL DEFAULT 0,
     bio VARCHAR(150),
@@ -54,7 +53,7 @@ create table volunteers (
 
 create table availability (
 	availability_id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_volunteer_id VARCHAR(255) NOT NULL,
+    fk_volunteer_id CHAR(36) NOT NULL,
     day INT NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
@@ -63,7 +62,7 @@ create table availability (
 );
 
 CREATE TABLE volunteer_schedule (
-    fk_volunteer_id VARCHAR(255) NOT NULL,
+    fk_volunteer_id CHAR(36) NOT NULL,
     fk_schedule_id INT NOT NULL,
     PRIMARY KEY (fk_volunteer_id, fk_schedule_id),
     FOREIGN KEY (fk_volunteer_id) REFERENCES volunteers(volunteer_id)
@@ -85,7 +84,7 @@ create table schedule (
 
 CREATE TABLE shifts (
     shift_id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_volunteer_id VARCHAR(255) NOT NULL, -- shifts always belong to a volunteer
+    fk_volunteer_id CHAR(36) NOT NULL, -- shifts always belong to a volunteer
     fk_schedule_id INT NOT NULL, -- shifts always belong to a schedule
     shift_date DATE NOT NULL,
     duration INT NOT NULL,
@@ -96,39 +95,43 @@ CREATE TABLE shifts (
         ON DELETE CASCADE
 );
 
-CREATE TABLE shift_coverage_request (
+CREATE TABLE absence_request (
     request_id INT PRIMARY KEY AUTO_INCREMENT,                     
-    fk_shift_id INT NOT NULL,                       
-    covered_by VARCHAR(255),
+    fk_shift_id INT NOT NULL,
+    approved BOOLEAN NOT NULL DEFAULT FALSE,
+    category ENUM('emergency', 'health', 'conflict', 'transportation', 'other') NOT NULL,
+    details VARCHAR(250) NOT NULL,
+    comments VARCHAR(150),            
+    covered_by CHAR(36),
     FOREIGN KEY (fk_shift_id)
         REFERENCES shifts(shift_id) ON DELETE CASCADE,
     FOREIGN KEY (covered_by)
         REFERENCES volunteers(volunteer_id) ON DELETE SET NULL
 );
 
-CREATE TABLE pending_shift_coverage (
+CREATE TABLE coverage_request (
     request_id INT NOT NULL,
-    pending_volunteer VARCHAR(255) NOT NULL,
+    volunteer_id CHAR(36) NOT NULL,
     
-    PRIMARY KEY (request_id, pending_volunteer),
-    FOREIGN KEY (request_id) REFERENCES shift_coverage_request(request_id) 
+    PRIMARY KEY (request_id, volunteer_id),
+    FOREIGN KEY (request_id) REFERENCES absence_request(request_id) 
         ON DELETE CASCADE,
-    FOREIGN KEY (pending_volunteer) REFERENCES volunteers(volunteer_id) 
+    FOREIGN KEY (volunteer_id) REFERENCES volunteers(volunteer_id) 
         ON DELETE CASCADE
 );
 
 CREATE TABLE class_preferences (
-    fk_volunteer_id VARCHAR(255), 
-    fk_class_id INT,        
+    fk_volunteer_id CHAR(255), 
+    fk_schedule_id INT,        
     class_rank INT,     
     FOREIGN KEY (fk_volunteer_id) REFERENCES volunteers(volunteer_id)
         ON DELETE CASCADE,
-    FOREIGN KEY (fk_class_id) REFERENCES class(class_id)
+    FOREIGN KEY (fk_schedule_id) REFERENCES schedule(schedule_id)
         ON DELETE CASCADE
 );
 
 create table images (
-    image_id VARCHAR(36) PRIMARY KEY,
+    image_id CHAR(36) PRIMARY KEY,
     image MEDIUMBLOB NOT NULL
 );
 
@@ -136,8 +139,8 @@ create table images (
 DROP TABLE IF EXISTS volunteer_schedule;
 DROP TABLE IF EXISTS availability;
 DROP TABLE IF EXISTS class_preferences;
-DROP TABLE IF EXISTS pending_shift_coverage;
-DROP TABLE IF EXISTS shift_coverage_request;
+DROP TABLE IF EXISTS coverage_request;
+DROP TABLE IF EXISTS absence_request;
 DROP TABLE IF EXISTS shifts;
 DROP TABLE IF EXISTS volunteers;
 DROP TABLE IF EXISTS schedule;

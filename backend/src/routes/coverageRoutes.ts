168bf7024cc3ba6996f6610906f0693f901ff850
Alec Situ <aasitu@gmail.com>
2025-03-15 01:34:26 -0700

    Coverage requests frontend (#71)
    * abstract details panel to add admin side
    
    * create class details panel for admins
    
    * style admin details panel
    
    * show schedule summary above header only for volunteers (admins can already see full schedules below)
    
    * implement basic class updating
    
    * add error message
    
    * fix issue with unassigning empty array of volunteers
    
    * basic class updating
    
    * relocate image uploader
    
    * implement class image uploading
    
    * fix image object
    
    * add frequency to schedule model
    
    * add schedule frequency to db
    
    * add instructor field
    
    * show volunteers in schedule cards - updating not yet implemented
    
    * use routing to switch between edit mode and classes mode
    
    * implement scheduling adding
    
    * use light upload icon for when class images already exist
    
    * implement schedule deleting and error handling
    
    * pass class id via useLocation().state
    
    * allow empty schedules array in add class endpoint
    
    * basic class creation
    
    * minor tweaks
    
    * implement volunteer adding
    
    * switch to edit state after a successful class creation
    
    * implement volunteer deleting
    
    * auto focus on select element when add volunteer button is pressed
    
    * a few more small tweaks
    
    * delete classes
    
    * add left-right buttons to admin side
    
    * fix header cutoff issue
    
    * move fk_instructor_id from class table to schedules table
    
    * move instructors to schedules in frontend
    
    * add instructor emails
    
    * bug fixes from merge
    
    * wrap on smaller screen
    
    * initial api
    
    * temp commit for rebase
    
    * undo changes
    
    * coverage requests frontend
    
    * small shift fix
    
    * accidental push
    
    * changed api
    
    ---------
    
    Co-authored-by: jansm04 <jan.smailbegovic@gmail.com>
    Co-authored-by: theosiemensrhodes <theo.siemensrhodes@gmail.com>

---
+++ backend/src/routes/coverageRoutes.ts
@@ -0,0 +1,78 @@
+import { body, param } from 'express-validator';
+import { RouteDefinition } from "../common/types.js";
+import { isAdmin, isAuthorized } from '../config/authCheck.js';
+import {
+  approveAbsenceRequest,
+  approveCoverShift,
+  rejectAbsenceRequest,
+  rejectCoverShift,
+  requestCoverShift,
+  withdrawAbsenceRequest,
+  withdrawCoverShift,
+  getAbsenceRequests,
+  getCoverageRequests,
+} from "../controllers/coverageController.js";
+
+export const CoverageRoutes: RouteDefinition = {
+  path: "/absence/:request_id",
+  middleware: [isAuthorized, isAdmin],
+  validation: [param("request_id").isInt({ min: 0 })],
+  children: [
+    {
+      path: "/",
+      method: "get",
+      action: getAbsenceRequests,
+    },
+    {
+      path: "/coverage",
+      method: "get",
+      action: getCoverageRequests,
+    },
+    {
+      path: "/approve",
+      method: "patch",
+      middleware: [isAdmin],
+      action: approveAbsenceRequest,
+    },
+    {
+      path: "/reject",
+      method: "delete",
+      middleware: [isAdmin],
+      action: rejectAbsenceRequest,
+    },
+    {
+      path: "/withdraw",
+      method: "delete",
+      action: withdrawAbsenceRequest,
+    },
+    {
+      path: "/coverage",
+      method: "post",
+      validation: [body("volunteer_id").isUUID()],
+      action: requestCoverShift,
+    },
+    {
+      path: "/coverage/:volunteer_id",
+      validation: [param("volunteer_id").isUUID()],
+      children: [
+        {
+          path: "/approve",
+          method: "patch",
+          middleware: [isAdmin],
+          action: approveCoverShift,
+        },
+        {
+          path: "/reject",
+          method: "delete",
+          middleware: [isAdmin],
+          action: rejectCoverShift,
+        },
+        {
+          path: "/withdraw",
+          method: "delete",
+          action: withdrawCoverShift,
+        },
+      ],
+    },
+  ],
+};

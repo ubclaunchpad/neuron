4795c8be23717d43fdee0150ab33b614a7eda30c
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-03-16 20:23:12 -0700

    use docker secrets and pwd files

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,14 +1,8 @@
-import { config } from "dotenv";
 import mysql, { Pool, PoolOptions } from 'mysql2/promise';
-
-config();
+import { DB_URI } from "./environment.js";
 
 const configuration: PoolOptions = {
-  host: process.env.RDS_HOSTNAME || '',
-  user: process.env.RDS_USERNAME || '',
-  password: process.env.RDS_PASSWORD || '',
-  database: process.env.RDS_DB || '',
-  port: process.env.RDS_PORT ? parseInt(process.env.RDS_PORT, 10) : 3306, // Default to port 3306 if not specified
+  uri: DB_URI, // from config.ts
   typeCast: (_, next) => {
     // Turn null columns into undefined
     const value = next();

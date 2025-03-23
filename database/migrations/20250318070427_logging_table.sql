a17b30888c69d95f05dc86aa522e9f12994f1b7b
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-03-22 20:55:24 -0700

    fix table migration

--- database/migrations/20250318070427_logging_table.sql
+++ database/migrations/20250318070427_logging_table.sql
@@ -1,11 +1,11 @@
 -- migrate:up
 CREATE TABLE log (
-    request_id INT PRIMARY KEY AUTO_INCREMENT,
+    log_id INT PRIMARY KEY AUTO_INCREMENT,
     page VARCHAR(30) NOT NULL,
     signoff VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     fk_volunteer_id CHAR(36), 
-    fk_class_id INT, 
+    fk_class_id INT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 
     FOREIGN KEY (fk_volunteer_id) REFERENCES volunteers(volunteer_id),

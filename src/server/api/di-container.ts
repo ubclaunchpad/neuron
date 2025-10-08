0129e52df9bd80b542dd994b0e0c94e10a779ea6
talia feng <88052205+talfee@users.noreply.github.com>
2025-10-07 21:11:16 -0400

    Create delete shifts (#116)
    * skeleton code
    
    * add shift functionality
    
    * delete shift function
    
    * tests run and are successful
    
    * unhardcode tests
    
    * delete shift directly
    
    * Stop tracking src/test.ts
    
    * get rid of courseId parameter when creating shift
    
    * get rid of courseId parameter when creating shift
    
    * small fixes before merge
    
    ---------
    
    Co-authored-by: edlng <edwardliangedli@gmail.com>
    Co-authored-by: theosiemensrhodes <theo.siemensrhodes@gmail.com>

--- src/server/api/di-container.ts
+++ src/server/api/di-container.ts
@@ -13,6 +13,7 @@ import { CacheService } from "../services/cacheService";
 import { EmailService } from "../services/emailService";
 import { ClassService } from "../services/entity/classService";
 import { InstructorService } from "../services/entity/instructorService";
+import { ShiftService } from "../services/entity/shiftService";
 import { TermService } from "../services/entity/termService";
 import { VolunteerService } from "../services/entity/volunteerService";
 
@@ -34,6 +35,7 @@ export type NeuronCradle = {
   instructorService: InstructorService;
   volunteerService: VolunteerService;
   termService: TermService;
+  shiftService: ShiftService;
 };
 
 export type NeuronContainer = AwilixContainer<NeuronCradle>;
@@ -59,6 +61,7 @@ const registerServices = (container: NeuronContainer) => {
   container.register({
     emailService: asClass<EmailService>(EmailService).singleton(),
     classService: asClass<ClassService>(ClassService).singleton(),
+    shiftService: asClass<ShiftService>(ShiftService).singleton(),
     instructorService: asClass<InstructorService>(InstructorService).singleton(),
     volunteerService: asClass<VolunteerService>(VolunteerService).singleton(),
     termService: asClass<TermService>(TermService).singleton(),

68905012a59ce6ccf49dc12dbd19cb71bda2f04f
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2026-01-29 22:27:16 -0800

    feat: package creation setup feat jjohngrey

--- src/env.js
+++ src/env.js
@@ -8,30 +8,22 @@ export const env = createEnv({
    * Server-side env vars
    */
   server: {
+    DATABASE_URL: z.url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-
-    // Database
-    DATABASE_URL: z.url(),
-    DATABASE_PASSWORD: z.string(),
-
-    // Redis
-    REDIS_URL: z.url(),
-    REDIS_PASSWORD: z.string(),
-
-    // Email
+    BETTER_AUTH_SECRET: z.string().min(32),
     SMTP_HOST: z.string(),
     SMTP_PORT: port,
     SMTP_USER: z.string(),
     SMTP_PASS: z.string(),
     MAIL_FROM: z.string(),
-
-    // MinIO
+    REDIS_URL: z.url(),
     MINIO_ROOT_USER: z.string(),
     MINIO_ROOT_PASSWORD: z.string(),
     MINIO_HOST: z.string(),
     MINIO_PORT: port,
+    MINIO_REGION: z.string(),
     MINIO_BUCKET: z.string(),
     MINIO_USE_SSL: z.enum(["true", "false"]).transform((val) => val === "true")
   },
@@ -48,24 +40,20 @@ export const env = createEnv({
    * Raw runtime env mapping
    */
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-
     DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-
-    REDIS_URL: process.env.REDIS_URL,
-    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-
+    NODE_ENV: process.env.NODE_ENV,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     SMTP_HOST: process.env.SMTP_HOST,
-    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_PORT: Number(process.env.SMTP_PORT),
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
     MAIL_FROM: process.env.MAIL_FROM,
-
+    REDIS_URL: process.env.REDIS_URL,
     MINIO_ROOT_USER: process.env.MINIO_ROOT_USER,
     MINIO_ROOT_PASSWORD: process.env.MINIO_ROOT_PASSWORD,
     MINIO_HOST: process.env.MINIO_HOST,
     MINIO_PORT: process.env.MINIO_PORT,
+    MINIO_REGION: process.env.MINIO_REGION,
     MINIO_BUCKET: process.env.MINIO_BUCKET,
     MINIO_USE_SSL: process.env.MINIO_USE_SSL,
 

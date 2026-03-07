4fba08e4372732a693a4bfb83a9393e1453ad5bb
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2026-03-07 12:49:38 -0800

    fix: remove One-on-One Exercise

--- src/components/classes/constants.ts
+++ src/components/classes/constants.ts
@@ -1,13 +1,12 @@
 export const CLASS_CATEGORIES = [
-    "Online Exercise",
-    "Creative & Expressive",
-    "Care Partner Workshops",
-    "Food & Nutrition",
-    "In-Person Exercise",
-    "One-on-One Exercise",
-    "IMPACT 360",
-    "Other Opportunities",
+  "Online Exercise",
+  "Creative & Expressive",
+  "Care Partner Workshops",
+  "Food & Nutrition",
+  "In-Person Exercise",
+  "IMPACT 360",
+  "Other Opportunities",
 ] as const;
 
 export const DEFAULT_CLASS_CATEGORY: (typeof CLASS_CATEGORIES)[number] =
-    "Other Opportunities";
+  "Other Opportunities";
